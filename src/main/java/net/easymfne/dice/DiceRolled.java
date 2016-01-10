32fb8df5fd5d589d57ca6825f13011381d3ce8a1
Martin Ambrus <martin@martinambrus.com>
2016-01-10 19:38:15 +0100

    forgot to commit the custom DiceRolled permission class file in the last commit

---
+++ src/main/java/net/easymfne/dice/DiceRolled.java
@@ -0,0 +1,54 @@
+/*
+ * This file is part of the Dice plugin by EasyMFnE.
+ *
+ * Dice is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * Dice is distributed in the hope that it will be useful, but without any
+ * warranty; without even the implied warranty of merchantability or fitness for
+ * a particular purpose. See the GNU General Public License for details.
+ *
+ * You should have received a copy of the GNU General Public License v3 along
+ * with Dice. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.easymfne.dice;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+/**
+ * Implements custom DiceRolled event, so other plugins can possibly listen
+ * to the result of this plugin's dice throw.
+ *
+ * @author Martin Ambrus
+ */
+public final class DiceRolled extends Event {
+
+	private static final HandlerList handlers = new HandlerList();
+    private String message;
+    private Integer[] numbers;
+
+    public DiceRolled(String event, Integer[] rolled) {
+        message = event;
+        numbers = rolled;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Integer[] getNumbersRolled() {
+    	return numbers;
+    }
+
+    @Override
+	public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+}
